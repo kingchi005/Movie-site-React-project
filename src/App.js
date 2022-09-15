202ec1d8d2988dd0500a9c309c25ff9484daed26
Kingchi <kingchi005@gmail.com>
2022-09-15 17:12:49 +0100

    "yes"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
+
+const App = () => {
+ const [num, setNum] = useState('0')
+  useEffect(() => {
+    setNum(100)
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e) => setNum((prevNum) => prevNum - 1)}>-</button>
+      <h1>Num: {num} </h1>
+      <button onClick={(e) => setNum((prevNum) => prevNum + 1)}>+</button>
     </div>
   );
 }
